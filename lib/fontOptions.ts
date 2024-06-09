5c0ec3004c526a1c7bb1ea2950c75e374ec03ce5
nathanmdj <nathanmdj628@gmail.com>
2024-06-10 01:49:45 +0800

    feat: /lib created font array

---
+++ lib/fontOptions.ts
@@ -0,0 +1,46 @@
+export const fontOptions = [
+  {
+    name: 'Inter',
+    font: 'font-inter',
+  },
+  {
+    name: 'Roboto Condensed',
+    font: 'font-roboto-condensed',
+  },
+  {
+    name: 'Racing Sans One',
+    font: 'font-racing-sans-one',
+  },
+  {
+    name: 'League Spartan',
+    font: 'font-league-spartan',
+  },
+  {
+    name: 'Montserrat',
+    font: 'font-montserrat',
+  },
+  {
+    name: 'Open Sans',
+    font: 'font-open-sans',
+  },
+  {
+    name: 'Raleway',
+    font: 'font-raleway',
+  },
+  {
+    name: 'Bebas Neue',
+    font: 'font-bebas',
+  },
+  {
+    name: 'Lato',
+    font: 'font-lato',
+  },
+  {
+    name: 'Kaushan Script',
+    font: 'font-kaushan',
+  },
+  {
+    name: 'Comic Neue',
+    font: 'font-comic-neue',
+  },
+] as const;
