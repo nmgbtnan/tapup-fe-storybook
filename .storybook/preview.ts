dc80e43ac3630bc76b5f53a8969917be6fb6e23c
reyesprince31 <reyes.prince31@gmail.com>
2024-06-12 18:25:31 +0800

    feat: storybook main.ts and preview.ts upon init

---
+++ .storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+import '../app/globals.css';
+
+const preview: Preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  tags: ["autodocs"]
+};
+
+export default preview;
