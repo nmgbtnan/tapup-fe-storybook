c26826024b7243315688b71cc4ec80b12b012f8c
nathanmdj <nathanmdj628@gmail.com>
2024-06-12 19:07:19 +0800

    feat: added updateSocial action on zustand state to update the social link instead of creating a new one if it already exists

--- hooks/useBuilderSocial.tsx
+++ hooks/useBuilderSocial.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import { FaFacebook } from 'react-icons/fa';
 import { create } from 'zustand'
 
-interface Social {
+export interface Social {
   name: string;
   icon: JSX.Element;
   link: string;
@@ -17,12 +18,21 @@ interface state {
 interface action {
   addSocial(name: string, icon: JSX.Element, link: string, iconColor: string, iconBgColor: string): void;
   removeSocial(name: string): void;
+  updateSocial(name: string, icon: JSX.Element, link: string, iconColor: string, iconBgColor: string): void;
 }
 
 export const useBuilderSocial = create<state & action>((set) => ({
-  socials: [  ],
+  socials: [],
   addSocial: (name: string, icon: JSX.Element, link: string, iconColor: string, iconBgColor: string) => set((state) => ({
     socials: [...state.socials, { name, icon, link, iconColor, iconBgColor }]
+  })),
+  updateSocial: (name: string, icon: JSX.Element, link: string, iconColor: string, iconBgColor: string) => set((state) => ({
+    socials: state.socials.map((social) => {
+      if (social.name === name) {
+        return { ...social, icon, link, iconColor, iconBgColor }
+      }
+      return social
+    })
   })),
   removeSocial: (name: string) => set((state) => ({
     socials: state.socials.filter((social) => social.name !== name)
