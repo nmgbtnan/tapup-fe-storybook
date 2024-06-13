0bf446076343864f473961d0bcf47eed2ca07312
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-13 11:38:06 +0800

    refactor: Replace Img with icon

--- components/FrontPage/ShowCaseCard.tsx
+++ components/FrontPage/ShowCaseCard.tsx
@@ -1,18 +1,20 @@
+import { TbAccessPoint } from "react-icons/tb";
+
 const showCaseCard = [
   {
-    title: 'Access Card',
+    title: "Access Card",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
   {
-    title: 'Access Card',
+    title: "Access Card",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
   {
-    title: 'Access Card',
+    title: "Access Card",
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
   },
 ];
 
@@ -21,11 +23,11 @@ export default function ShowCaseCard() {
     <div className="mt-16 flex flex-col justify-center gap-12 md:flex-row md:gap-8">
       {showCaseCard.map((c, i) => (
         <div className=" flex gap-4 md:flex-wrap lg:flex-nowrap " key={i}>
-          <span
-            className={`flex aspect-square h-8 w-8 items-center justify-center rounded-lg bg-custom-black md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-14 ${i === 1 && 'bg-custom-green'}`}
+          <div
+            className={`flex aspect-square h-8 w-8  items-center justify-center  rounded-lg bg-custom-black md:h-10 md:w-10 lg:h-12 lg:w-12 xl:h-14 xl:w-32 ${i === 1 && "bg-custom-green"}`}
           >
-            <img src="./sound-icon.svg" alt="sound-icon" />
-          </span>
+            <TbAccessPoint className="text-white md:w-full h-8 w-8 md:h-full " />
+          </div>
 
           <div className="flex flex-col space-y-2">
             <span className="text-sm font-semibold md:text-xl lg:text-2xl xl:text-3xl">
