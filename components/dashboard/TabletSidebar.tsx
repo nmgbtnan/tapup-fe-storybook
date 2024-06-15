4d0df168bb48e0345e96ad1490af090a30b704ad
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-15 09:41:44 +0800

    refactor: add next js image component and image container

--- components/dashboard/TabletSidebar.tsx
+++ components/dashboard/TabletSidebar.tsx
@@ -1,13 +1,15 @@
-import { SIDEBAR_LINKS } from '@/lib/dashboardLinks';
-import Link from 'next/link';
-
+import { SIDEBAR_LINKS } from "@/lib/dashboardLinks";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function TabletSidebar() {
   return (
     <aside className="fixed z-50 hidden min-h-screen bg-custom-black md:block lg:hidden">
       <nav className="flex h-dvh w-16 flex-col  p-4">
         <div className="mb-16 flex justify-center">
-          <img src="../../T-logo.svg" alt="Tapup Logo" />
+          <div className="relative">
+            <Image src="./T-logo.svg" alt="Tapup Logo" fill />
+          </div>
         </div>
 
         <ul className="flex flex-1 flex-col items-center gap-8 text-white">
@@ -15,7 +17,7 @@ export default function TabletSidebar() {
             <Link
               href={el.path}
               key={el.key}
-              className={`${el.key === 'settings' && 'mt-auto'}`}
+              className={`${el.key === "settings" && "mt-auto"}`}
             >
               {el.icon}
             </Link>
