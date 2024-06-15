9097f86e19cf9da9cb822ed9e04add9075c578f1
nathanmdj <nathanmdj628@gmail.com>
2024-06-15 22:38:21 +0800

    feat: created a useEffect function to getData from localstorage then set it as initialValues of builder states

--- components/builder/BuilderContainer.tsx
+++ components/builder/BuilderContainer.tsx
@@ -3,17 +3,26 @@ import Preview from '@/components/builder/Preview'
 import Sidebar from '@/components/builder/Sidebar'
 import { useBuilderProfile } from '@/hooks/useBuilderProfile'
 import getToken from '@/lib/auth/getToken'
+import { createCardProfile, getCardProfile } from '@/lib/localStorage'
 import { useEffect } from 'react'
 const BuilderContainer = () => {
-  const {changeName, name} = useBuilderProfile((state) => state)
-  
-  const updateInitialVal = () => {
-    getToken().then((response) => changeName(response.user.name))
-  }
-
+  const {changeName, changePosition, changeBio, changeProfilePhoto, changeCoverPhoto} = useBuilderProfile((state) => state)
+ 
   useEffect(() => {
-    if(name === '')
-    updateInitialVal();
+    if(!getCardProfile('card')){
+      createCardProfile();
+    } else if(getCardProfile('card').hasData) {
+      const data = getCardProfile('card');
+      changeName(data.profile.name);
+      changePosition(data.profile.position);
+      changeBio(data.profile.bio);
+      changeProfilePhoto(data.profile.profilePhoto);
+      changeCoverPhoto(data.profile.coverPhoto);
+    } else {
+      getToken().then((response) => changeName(response.user.name))
+      console.log("getting token");
+      
+    }
   }, [])
 
   return (
